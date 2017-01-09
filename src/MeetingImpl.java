879e7d7a99a75636ac6f37ff3f10bb1af0d90db8
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-09 23:35:54 +0000

    Added constructor to MeetingImpl

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -6,6 +6,29 @@ import java.util.Set;
  */
 public abstract class MeetingImpl implements Meeting {
 
+    private int id;
+    private Calendar date;
+    private Set<Contact> contacts;
+
+    /**
+     *
+     * @param id
+     * @param date
+     * @param contacts
+     * @throws IllegalArgumentException when id <=0 or contacts is empty
+     * @throws NullPointerException when either date or contacts are null
+     */
+    public MeetingImpl(int id, Calendar date, Set<Contact> contacts)
+                       throws IllegalArgumentException, NullPointerException {
+        this.id = id;
+        this.date = date;
+        this.contacts = contacts;
+
+        if (date == null || contacts == null)
+            throw new NullPointerException("date or contact set are null!");
+        if (id <= 0 || contacts.size() == 0)
+            throw new IllegalArgumentException("Either id <=0 or the contacts set is empty!");
+    }
 
     public abstract int getId();
 
