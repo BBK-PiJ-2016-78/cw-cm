a5f20593cc81851135a135400592b02df2936107
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-09 23:55:19 +0000

    Added basic implementation class for PastMeeting, to serve for testing

---
+++ src/PastMeetingImpl.java
@@ -0,0 +1,38 @@
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * Created by hradev01 on 09-Jan-17.
+ */
+public class PastMeetingImpl {
+
+    private int id;
+    private Calendar date;
+    private Set<Contact> contacts;
+    private String notes = "";
+
+    /**
+     *
+     * @param id
+     * @param date
+     * @param contacts
+     * @param notes
+     * @throws IllegalArgumentException when id <=0 or contacts is empty
+     * @throws NullPointerException when either date or contacts are null
+     */
+    public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts, String notes)
+            throws IllegalArgumentException, NullPointerException {
+        this.id = id;
+        this.date = date;
+        this.contacts = contacts;
+        this.notes = notes;
+        if (date == null || contacts == null || notes == null)
+            throw new NullPointerException("date or contact set are null!");
+        if (id <= 0 || contacts.size() == 0)
+            throw new IllegalArgumentException("Either id <=0 or the contacts set is empty!");
+    }
+
+    public String getNotes(){
+        return null;
+    }
+}
