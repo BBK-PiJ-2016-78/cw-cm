5814be04597e1a904c02faf0e2a8814aee80ebc9
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-09 21:34:05 +0000

    Added new border case test to ContactImplTest

--- src/ContactImplTest.java
+++ src/ContactImplTest.java
@@ -10,21 +10,32 @@ class ContactImplTest {
     Contact contacts = new ContactImpl(1, "name", "");
 
     @Test
-    void getId() {
+    void getIdTest() {
         int output = contacts.getId();
         int expected = 1;
         assertEquals(expected, output);
     }
 
     @Test
-    void getName() {
+    void TestBorders() {
+        try {
+            Contact border = new ContactImpl(0, null);
+            fail("Expected IllegalArgumentException or NullPointerException");
+        } catch (IllegalArgumentException | NullPointerException ex) {
+            // if caught test Pass
+        }
+
+    }
+
+    @Test
+    void getNameTest() {
         String output = contacts.getName();
         String expected = "name";
         assertEquals(expected, output);
     }
 
     @Test
-    void getNotes() {
+    void getNotesTest() {
         String output = contacts.getNotes();
         String expected = "";
         assertEquals(expected, output);
