264eb5495bb93fb513828f2e4134e1f9a7903a43
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-09 19:49:06 +0000

    Added ContactManager Test

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -2,4 +2,21 @@
  * Created by hradev01 on 09-Jan-17.
  */
 public class ContactImpl implements Contact {
+
+
+    public int getId(){
+        return 0;
+    }
+
+    public String getName(){
+        return null;
+    }
+
+    public String getNotes(){
+        return null;
+    }
+
+    public void addNotes(String note){
+
+    }
 }
