8b705ae9baa2254e795e4e9fcd4ddffa9f3710f3
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-09 20:44:46 +0000

    Fully implemented the Contact interface

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -3,20 +3,67 @@
  */
 public class ContactImpl implements Contact {
 
+    private int id;
+    private String name;
+    private String notes = "";
 
+    /**
+     *
+     * @param id the ID of the contact
+     * @param name the name of the contact
+     * @param notes the notes to be added
+     */
+    public ContactImpl(int id, String name, String notes){
+        this.id = id;
+        this.name = name;
+        this.notes = notes;
+    }
+
+    /**
+     *
+     * @param id the ID of the contact
+     * @param name the name of the contact
+     * @throws IllegalArgumentException when the id argument is 0 or negative
+     * @throws NullPointerException when the passed name argument is a null
+     */
+    public ContactImpl(int id, String name) throws IllegalArgumentException, NullPointerException {
+        this.id = id;
+        if (id <= 0)
+            throw new IllegalArgumentException("The ID is 0 or e negative value!");
+        this.name = name;
+        if (name == null)
+            throw new NullPointerException("The name value is null!");
+    }
+
+    /**
+     *
+     * @return the ID of the contact
+     */
     public int getId(){
-        return 0;
+        return id;
     }
 
+    /**
+     *
+     * @return the name of the contact
+     */
     public String getName(){
-        return null;
+        return name;
     }
 
+    /**
+     *
+     * @return a string with notes about the contact, if none, return empty string
+     */
     public String getNotes(){
-        return null;
+        return notes;
     }
 
+    /**
+     *
+     * @param note the notes to be added
+     */
     public void addNotes(String note){
-
+        notes = note;
     }
 }
