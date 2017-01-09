b0a15c81437a762f1866bf075aa5c0cd2093eb52
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-09 19:49:56 +0000

    Added ContactManager Test

---
+++ src/ContactImplTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by hradev01 on 09-Jan-17.
+ */
+class ContactImplTest {
+    @Test
+    void getId() {
+
+    }
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void getNotes() {
+
+    }
+
+    @Test
+    void addNotes() {
+
+    }
+
+}
