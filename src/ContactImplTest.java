ca3495914d381a4713b091bfb5061b8897c8cb7e
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-09 20:14:27 +0000

    Added ContactManager  and Contact Tests

--- src/ContactImplTest.java
+++ src/ContactImplTest.java
@@ -6,24 +6,33 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by hradev01 on 09-Jan-17.
  */
 class ContactImplTest {
+
+    Contact contacts = new ContactImpl();
+
     @Test
     void getId() {
-
+        int output = contacts.getId();
+        int expected = 1;
+        assertEquals(expected, output);
     }
 
     @Test
     void getName() {
-
+        String output = contacts.getName();
+        String expected = "name";
+        assertEquals(expected, output);
     }
 
     @Test
     void getNotes() {
-
+        String output = contacts.getNotes();
+        String expected = "";
+        assertEquals(expected, output);
     }
 
-    @Test
+   /* @Test
     void addNotes() {
 
-    }
+    }*/
 
 }
