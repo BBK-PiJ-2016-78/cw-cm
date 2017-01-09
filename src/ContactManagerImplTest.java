2b1b6c81c5f45c7928b4ce5eecd723a1ab3c22d0
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-09 19:21:06 +0000

    Added ContactManager Test

---
+++ src/ContactManagerImplTest.java
@@ -0,0 +1,68 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+/**
+ * Created by hradev01 on 09-Jan-17.
+ */
+class ContactManagerImplTest {
+
+    @Test
+    void addFutureMeeting() {
+
+    }
+
+    @Test
+    void getPastMeeting() {
+
+    }
+
+    @Test
+    void getFutureMeeting() {
+
+    }
+
+    @Test
+    void getFutureMeetingList() {
+
+    }
+
+    @Test
+    void getMeetingListOn() {
+
+    }
+
+    @Test
+    void getPastMeetingListFor() {
+
+    }
+
+    @Test
+    void addNewPastMeeting() {
+
+    }
+
+    @Test
+    void addMeetingNotes() {
+
+    }
+
+    @Test
+    void addNewContact() {
+
+    }
+
+    @Test
+    void getContacts() {
+
+    }
+
+    @Test
+    void getContacts1() {
+
+    }
+
+    @Test
+    void flush() {
+
+    }
+
+}
