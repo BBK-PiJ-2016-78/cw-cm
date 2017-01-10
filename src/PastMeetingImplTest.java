6806753744e906ab24fa276b00e1d40445cf4b74
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-10 00:18:25 +0000

    Added test for PastMeeting to fail as PastMeetingImpl is only mock so far

---
+++ src/PastMeetingImplTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by hradev01 on 09-Jan-17.
+ */
+class PastMeetingImplTest {
+
+    @Test
+    void getNotesTest() {
+        Calendar date = new GregorianCalendar(2017, 01, 10);
+        Set<Contact> contacts = new TreeSet<Contact>();
+        Contact contact = new ContactImpl(1, "name", "");
+        contacts.add(contact);
+        PastMeeting meet = (PastMeeting) new PastMeetingImpl(1, date, contacts, "");
+
+        String output = meet.getNotes();
+        String expected = "";
+        assertEquals(expected, output);
+    }
+
+}
