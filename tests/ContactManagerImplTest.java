c0d33d3fe82cb5ac1cc7cd4b8840ccb77fe582cc
BBK-PiJ-2016-78 <chsrdly@gmail.com>
2017-01-12 19:54:43 +0000

    Added test for addFutureMeeting that fails as expected

--- tests/ContactManagerImplTest.java
+++ tests/ContactManagerImplTest.java
@@ -8,13 +8,23 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
 
 class ContactManagerImplTest {
 
+    Calendar date = new GregorianCalendar(2017, 01, 12);
+    Set<Contact> contacts = new HashSet<>();
+    Contact contact = new ContactImpl(1, "name", "stuff");
 
     @Test
     void addFutureMeetingTest() {
-
+        contacts.add(contact);
+        ContactManager manager = new ContactManagerImpl();
+        int output = manager.addFutureMeeting(contacts, date);
+        int expected = 1;
+        assertEquals(expected, output);
     }
 
     @Test
